8e5fccc5880f847b84dedc8bf445c4366fecba00
ABHISHEK VERMA <12abhishekverma13@gmail.com>
2021-12-28 01:06:24 +0530

    finalo

--- FruityDelight/settings.py
+++ FruityDelight/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['fruitydelight.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['fruitydelight.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
